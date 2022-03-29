cfafb8a211e9de021e65478dac8da09f897eeb44
kHill003 <kris.hillquist@codeclan.com>
2022-03-29 11:15:43 +0100

    my commit

---
+++ logical_operators.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+
+jarrod_hungery = True
+jarrod_tired   = False
+
+if jarrod_hungery and jarrod_tired:
+    print("jarrod is angry")
+if jarrod_tired or jarrod_hungery:
+    print('jarrod is grumpy')
---
+++ what_animal.py
@@ -0,0 +1,17 @@
+# get the imput from the user, and if the animal is equal to a "chicken"
+ # then the display( print "that is my favorite animal")
+# end
+from ast import PyCF_ONLY_AST
+
+
+animal = input ("what animal are you")
+if animal == "chicken":
+    print("this is my dinner")
+
+elif animal == "pony":
+    print("I have a pony!!!!")
+else:
+    print("I am going to zoo")
+
+
+
